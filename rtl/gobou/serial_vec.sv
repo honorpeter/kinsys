3969cc774b2632e30f951581140f0e02b2b1c45e
Takayuki Ujiie <kaftulal@hotmail.co.jp>
2017-01-27 12:04:41 +0900

    rtl/test: initiated test_serial_vec

--- rtl/gobou/serial_vec.sv
+++ rtl/gobou/serial_vec.sv
@@ -1,21 +1,21 @@
 // TODO: Eliminate erb
 
 module serial_vec
-  ( input                     clk
-  , input                     xrst
-  , input                     serial_we;
-  <%- $core.times do |i| -%>
-  , input signed [DWIDTH-1:0] in_data<%=i%>;
-  <%- end -%>
+ #( parameter DWIDTH = 32
+  , parameter LWIDTH = 12
+  , parameter CORE   = 8
+  )
+  ( input                       clk
+  , input                       xrst
+  , input                       serial_we
+  , input  signed [DWIDTH-1:0]  in_data [CORE-1:0]
   , output signed [DWIDTH-1:0]  out_data
   );
 
   reg [LWIDTH-1:0]        r_cnt;
-  <%- $core.times do |i| -%>
-  reg signed [DWIDTH-1:0] r_data<%=i%>;
-  <%- end -%>
+  reg signed [DWIDTH-1:0] r_data [CORE-1:0];
 
-  assign out_data = r_data0;
+  assign out_data = r_data[0];
 
   always @(posedge clk or negedge xrst)
     if (!xrst)
@@ -28,9 +28,17 @@ module serial_vec
       else
         r_cnt <= r_cnt + 1;
 
-  for (genvar i=0; i < CORE; i++)
-    always @(posedge clk)
-      if (!xrst)
-        r_data
+    genvar i;
+    for (i = 0; i < CORE; i=i+1)
+      always @(posedge clk)
+        if (!xrst)
+          r_data[i] <= 0;
+        else if (serial_we)
+          r_data[i] <= in_data[i];
+        else
+          if (i == CORE -1)
+            r_data[i] <= 0;
+          else
+            r_data[i] <= r_data[i+1];
 
 endmodule
---
+++ rtl/test/gobou/test_serial_vec.sv
@@ -0,0 +1,62 @@
+`timescale 1ns/1ps
+
+module test_serial_vec ();
+  parameter DWIDTH = 32;
+  parameter LWIDTH = 12;
+  parameter CORE   = 8;
+  parameter STEP   = 10;
+  reg                      clk;
+  reg                      xrst;
+  reg                      serial_we;
+  reg signed [DWIDTH-1:0]  in_data [CORE-1:0];
+  reg signed [DWIDTH-1:0]  out_data;
+
+  serial_vec dut(.*);
+
+  // clock
+  initial begin
+    clk = 0;
+    forever
+      #(STEP/2) clk = ~clk;
+  end
+
+  // flow
+  initial begin
+    xrst = 0;
+    #STEP;
+    xrst = 1;
+    #STEP;
+    serial_we = 1;
+    for (int i = 0; i < CORE; i++)
+      in_data[i] = i;
+    #STEP;
+    serial_we = 0;
+    #(STEP*10);
+    $finish();
+  end
+
+  // display
+  always @(posedge clk) begin
+    #(STEP/2-1);
+    $display(
+      "%2d: ", $time/STEP,
+      "%d ", xrst,
+      "%d ", serial_we,
+      "| ",
+      "%2d ", in_data[0],
+      "%2d ", in_data[1],
+      "%2d ", in_data[2],
+      "%2d ", in_data[3],
+      "%2d ", in_data[4],
+      "%2d ", in_data[5],
+      "%2d ", in_data[6],
+      "%2d ", in_data[7],
+      "| ",
+      "%2d ", out_data,
+      "|"
+    );
+    #(STEP/2+1);
+  end
+
+endmodule
+
