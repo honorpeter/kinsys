b1ea605f81721b323b07678fcedc2fcacae40888
Takayuki Ujiie <takau@easter.kuee.kyoto-u.ac.jp>
2017-06-05 21:16:32 +0900

    vivado: block design for zcu102

--- vivado/bd.tcl
+++ vivado/bd.tcl
@@ -2,23 +2,42 @@
 
 create_bd_design "design_1"
 
-create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7_0
-create_bd_cell -type ip -vlnv user.org:user:${ip_name}:1.0 ${ip_name}_0
+if {$proj_name == "zcu102"} {
+  create_bd_cell -type ip -vlnv xilinx.com:ip:zynq_ultra_ps_e:2.0 zynq_ultra_ps_e_0
+  create_bd_cell -type ip -vlnv user.org:user:${ip_name}:1.0 ${ip_name}_0
 
-apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 \
-  -config {make_external "FIXED_IO, DDR" apply_board_preset "1" Master "Disable" Slave "Disable" } [get_bd_cells processing_system7_0]
+  apply_bd_automation -rule xilinx.com:bd_rule:zynq_ultra_ps_e -config {apply_board_preset "1" }  [get_bd_cells zynq_ultra_ps_e_0]
 
-switch $ip_name {
-  "kinpira_axi_lite" {
-    apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP0" Clk "Auto" }  [get_bd_intf_pins ${ip_name}_0/s_axi]
+  switch $ip_name {
+    "kinpira_axi_lite" {
+      apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/zynq_ultra_ps_e_0/M_AXI_HPM0_FPD" Clk "Auto" }  [get_bd_intf_pins ${ip_name}_0/s_axi]
+    }
+    "kinpira_axi" {
+      apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/zynq_ultra_ps_e_0/M_AXI_HPM0_FPD" Clk "Auto" }  [get_bd_intf_pins ${ip_name}_0/s_axi_params]
+      apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/zynq_ultra_ps_e_0/M_AXI_HPM1_FPD" Clk "Auto" }  [get_bd_intf_pins ${ip_name}_0/s_axi_gobou]
+      apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/zynq_ultra_ps_e_0/M_AXI_HPM1_FPD" Clk "Auto" }  [get_bd_intf_pins ${ip_name}_0/s_axi_image]
+      apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/zynq_ultra_ps_e_0/M_AXI_HPM1_FPD" Clk "Auto" }  [get_bd_intf_pins ${ip_name}_0/s_axi_renkon]
+    }
   }
-  "kinpira_axi" {
-    set_property -dict [list CONFIG.PCW_USE_M_AXI_GP1 {1}] [get_bd_cells processing_system7_0]
+} else {
+  create_bd_cell -type ip -vlnv xilinx.com:ip:processing_system7:5.5 processing_system7_0
+  create_bd_cell -type ip -vlnv user.org:user:${ip_name}:1.0 ${ip_name}_0
 
-    apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP0" Clk "Auto" }  [get_bd_intf_pins ${ip_name}_0/s_axi_params]
-    apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP1" Clk "Auto" }  [get_bd_intf_pins ${ip_name}_0/s_axi_image]
-    apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP1" Clk "Auto" }  [get_bd_intf_pins ${ip_name}_0/s_axi_renkon]
-    apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP1" Clk "Auto" }  [get_bd_intf_pins ${ip_name}_0/s_axi_gobou]
+  apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 \
+    -config {make_external "FIXED_IO, DDR" apply_board_preset "1" Master "Disable" Slave "Disable" } [get_bd_cells processing_system7_0]
+
+  switch $ip_name {
+    "kinpira_axi_lite" {
+      apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP0" Clk "Auto" }  [get_bd_intf_pins ${ip_name}_0/s_axi]
+    }
+    "kinpira_axi" {
+      set_property -dict [list CONFIG.PCW_USE_M_AXI_GP1 {1}] [get_bd_cells processing_system7_0]
+
+      apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP0" Clk "Auto" }  [get_bd_intf_pins ${ip_name}_0/s_axi_params]
+      apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP1" Clk "Auto" }  [get_bd_intf_pins ${ip_name}_0/s_axi_image]
+      apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP1" Clk "Auto" }  [get_bd_intf_pins ${ip_name}_0/s_axi_renkon]
+      apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {Master "/processing_system7_0/M_AXI_GP1" Clk "Auto" }  [get_bd_intf_pins ${ip_name}_0/s_axi_gobou]
+    }
   }
 }
 
