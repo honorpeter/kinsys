62261a3bc4a9ce4dc307248531b229f0a25f1031
Takayuki Ujiie <takau@easter.kuee.kyoto-u.ac.jp>
2018-03-01 19:50:20 +0900

    .

--- rtl/common/renkon.svh
+++ rtl/common/renkon.svh
@@ -7,34 +7,43 @@
 // User parameters ( $_ for substitution )
 ////////////////////////////////////////////////////////////
 `ifdef DIST
-parameter RENKON_CORE    = _;
-parameter RENKON_CORELOG = _;
-parameter RENKON_NETSIZE = _;
-parameter RENKON_MAXIMG  = _;
+parameter RENKON_CORE     = _;
+parameter RENKON_CORELOG  = _;
+parameter RENKON_NETSIZE  = _;
+parameter RENKON_IMGH_MAX = _;
+parameter RENKON_IMGW_MAX = _;
+parameter RENKON_FEAH_MAX = _;
+parameter RENKON_FEAW_MAX = _;
+parameter RENKON_OUTH_MAX = _;
+parameter RENKON_OUTW_MAX = _;
 `else
-parameter RENKON_CORE    = 8;
-parameter RENKON_CORELOG = 3;
-parameter RENKON_NETSIZE = 11;
-parameter RENKON_MAXIMG  = 32;
+parameter RENKON_CORE     = 8;
+parameter RENKON_CORELOG  = 3;
+parameter RENKON_NETSIZE  = 11;
+parameter RENKON_IMGH_MAX = 32;
+parameter RENKON_IMGW_MAX = 32;
+parameter RENKON_FEAH_MAX = 28;
+parameter RENKON_FEAW_MAX = 28;
+parameter RENKON_OUTH_MAX = 14;
+parameter RENKON_OUTW_MAX = 14;
 `endif
 
-// expected max featuremap size (cf. $clog2(24x24))
-parameter FEASIZE = $clog2(RENKON_MAXIMG**2);
-parameter OUTSIZE = $clog2(RENKON_MAXIMG**2);
 // parameter CONV_MAX   = 3;
 parameter CONV_MAX   = 5;
 // parameter POOL_MAX   = 2;
 parameter POOL_MAX   = 3;
 
+// expected max featuremap size (cf. $clog2(24x24))
+parameter FEASIZE = $clog2(RENKON_FEAH_MAX * RENKON_FEAW_MAX);
+parameter OUTSIZE = $clog2(RENKON_OUTH_MAX * RENKON_OUTW_MAX);
+
 ////////////////////////////////////////////////////////////
 // Delay of each modules
 ////////////////////////////////////////////////////////////
-// // max size (image height or width)
-parameter D_PIXELBUF = RENKON_MAXIMG;
-// // max size (image height or width)
-parameter D_POOLBUF  = RENKON_MAXIMG;
-// parameter D_PIXELBUF = 32;
-// parameter D_POOLBUF  = 32;
+// max size (image height or width)
+parameter D_PIXELBUF = RENKON_IMGW_MAX;
+// max size (image height or width)
+parameter D_POOLBUF  = RENKON_IMGW_MAX;
 // CONV_MAX = 5
 parameter D_CONV     = 5;
 // CONV_MAX = 3
